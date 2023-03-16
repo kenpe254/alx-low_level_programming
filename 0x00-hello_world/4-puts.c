103a4e69dc20aecf1667fe1b118b2aafb79ee46d
Kenpe254 <kennedygathuita@gmail.com>
2023-03-16 13:25:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 /**
- *  *main - entry point
- *   *return: always 0 (success)
- */
+ *  * main - entry point
+ *   *
+ *    * This function prints a message to the console and returns 0 on success.
+ *     *
+ *      * Return: Always 0 (success)
+*/
 int main(void)
 {
-	 puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
